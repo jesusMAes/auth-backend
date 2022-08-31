7798656a77b1e963252c58d3809262d17411b961
jesusMAes <jesusmarmolesp@gmail.com>
2022-08-31 22:42:19 +0200

    half tutorial made

---
+++ auth-frontend/react-auth/src/AuthComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function AuthComponent(){
+  return (
+    <div>
+      <h1>You have to be logged to reach me!</h1>
+    </div>
+  )
+}
---
+++ auth-frontend/react-auth/src/FreeComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function FreeComponent(){
+  return (
+    <div>
+      <h1>Everyone can access me!!</h1>
+    </div>
+  )
+}
